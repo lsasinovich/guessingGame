b121d3d60ca8bda43c437b7826c632db47c4abb6
Lizaveta <liza_sasinovich@mail.ru>
2017-09-22 23:15:44 +0200

    Add guessing Game class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        var max = 0;
+        var min = 0;
+    }
 
-    setRange(min, max) {
-
+    setRange (min, max) {
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return  Math.round((this.max-this.min)/2) + this.min;;
     }
 
     lower() {
-
+        this.max =  Math.round((this.max-this.min)/2) + this.min;
     }
 
     greater() {
-
+        this.min =  Math.round((this.max-this.min)/2) + this.min;
     }
 }
-
 module.exports = GuessingGame;
